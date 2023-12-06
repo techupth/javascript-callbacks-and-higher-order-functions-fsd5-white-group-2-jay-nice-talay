fbaa787f41ddda99809c0deb5306e4cc79641b09
Jaturapat Dusadee <mr.jaturapat.d@gmail.com>
2023-12-06 13:24:45 +0700

    finish exercise hackhour

--- ex-2.js
+++ ex-2.js
@@ -2,7 +2,19 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let result;
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (operation(array[i])) {
+      count += 1;
+    }
+  }
+  result = count >= 5 ? true : false;
+
+  return result;
 }
+
+const calculateScore = (array) => (array >= 70 ? true : false);
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
@@ -13,3 +25,11 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 let scoreRoom1Result;
 let scoreRoom2Result;
 let scoreRoom3Result;
+
+scoreRoom1Result = atLeastFive(studentScoresRoom1, calculateScore);
+scoreRoom2Result = atLeastFive(studentScoresRoom2, calculateScore);
+scoreRoom3Result = atLeastFive(studentScoresRoom3, calculateScore);
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
